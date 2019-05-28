e69ae7897d74d8e2477326b94b761aab2a43e233
Dylan Kan <42816443+Fanta67@users.noreply.github.com>
2019-05-28 13:00:46 -0500

    Starting JSON handling

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const app = document.getElementById('root');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://developer.nps.gov/api/v1/visitorcenters?&api_key=oeKLO4WwSs82lEaiPseSaWyx462T696oefty2fUS', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status == 200) {
+    data.data.forEach(center => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = center.name;
+
+      const p = document.createElement('p');
+      center.description = center.description.substring(0, 300);
+      p.textContent = `${center.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+}
+
+request.send();
